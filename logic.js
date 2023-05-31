cdedebe6d74438ba200a9c528abef3dbab942911
qertend <szucslevente607@gmail.com>
2023-05-31 20:18:17 +0200

    started Player class

--- logic.js
+++ logic.js
@@ -8,6 +8,31 @@ let keyBuffer = [];
 window.addEventListener("keydown", function(event)  {keyBuffer[event.key] = event.type == "keydown";});
 window.addEventListener("keyup", function(event)  {keyBuffer[event.key] = event.type == "keydown";});
 
+
+/**
+ * The class used to build Player objects
+ */
+class Player {
+    /**
+     * 
+     * @param {float} x the X coordinate of the start position
+     * @param {float} y the Y coordinate of the start position
+     * @param {object} controls an object with four properties representing the controlling keys: forward, backward, left, right
+     */
+    constructor(x, y, controls) {
+        this.x = x;
+        this.y = y;
+        this.forward = controls.forward;
+        this.backward = controls.backward;
+        this.left = controls.left;
+        this.right = controls.right;
+    }
+
+    update() {
+
+    }
+}
+
 /**
  * gets executed every frame
  * 
